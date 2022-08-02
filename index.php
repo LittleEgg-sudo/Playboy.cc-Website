9a6b4c5ba984bbb7e985f1bb275b42ab5f19c564
LittleEgg <62865583+LittleEgg-sudo@users.noreply.github.com>
2022-08-02 16:05:31 -0700

    Initial Upload

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<head>
+  <title>Playboy.cc</title>
+  <link rel="stylesheet" type="text/css" href="util.css">
+  <link rel="stylesheet" type="text/css" href="main.css">
+  <link rel="stylesheet" type="text/css" href="index.css">
+  <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+  <script class="u-script" type="text/javascript" src="index.js" defer=""></script>
+  <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+  <style>
+    h1 {
+      color: white;
+      text-align: center;
+    }
+  </style>
+</head>
+<body style="background-color: #1e1e1e; background-image: url('bg.gif'); background-size: contain;" class="">
+  <header class="u-align-right u-black u-clearfix u-header u-header" id="sec-fdbd" style="height: 132px;">
+    <div class="u-clearfix u-sheet u-sheet-1">
+      <a href="index.html" data-page-id="84612752" class="u-image u-logo u-image-1 animated customAnimationIn-played" data-image-width="2134" data-image-height="2134" data-animation-name="customAnimationIn" data-animation-duration="1000" data-animation-direction="" title="Home" style="will-change: transform, opacity;animation-duration: 1000ms;margin-left: 100px;">
+        <img src="untitled.png" class="u-logo-image u-logo-image-1">
+      </a>
+      <nav class="u-menu u-menu-one-level u-offcanvas u-menu-1" data-responsive-from="MD">
+        <div class="u-custom-menu u-nav-container">
+          <ul class="u-nav u-spacing-30 u-unstyled u-nav-1">
+            <li class="u-nav-item">
+              <a class="u-border-2 u-border-active-custom-color-1 u-border-hover-custom-color-1 u-border-no-left u-border-no-right u-border-no-top u-button-style u-nav-link u-text-active-white u-text-hover-white u-text-white" href="index.php" style="padding: 10px 0px;">Home</a>
+            </li>
+            <li class="u-nav-item">
+              <a class="u-border-2 u-border-active-custom-color-1 u-border-hover-custom-color-1 u-border-no-left u-border-no-right u-border-no-top u-button-style u-nav-link u-text-active-white u-text-hover-white u-text-white" href="https://playboycheats.sellix.io/" style="padding: 10px 0px;">Purchase</a>
+            </li>
+            <li class="u-nav-item">
+              <a class="u-border-2 u-border-active-custom-color-1 u-border-hover-custom-color-1 u-border-no-left u-border-no-right u-border-no-top u-button-style u-nav-link u-text-active-white u-text-hover-white u-text-white" href="status.php" style="padding: 10px 0px;">Status</a>
+            </li>
+          </ul>
+        </div>
+        <style class="offcanvas-style">
+          .u-offcanvas .u-sidenav {
+            flex-basis: 250px !important;
+          }
+
+          .u-offcanvas:not(.u-menu-open-right) .u-sidenav {
+            margin-left: -250px;
+          }
+
+          .u-offcanvas.u-menu-open-right .u-sidenav {
+            margin-right: -250px;
+          }
+
+          @keyframes menu-shift-left {
+            from {
+              left: 0;
+            }
+
+            to {
+              left: 250px;
+            }
+          }
+
+          @keyframes menu-unshift-left {
+            from {
+              left: 250px;
+            }
+
+            to {
+              left: 0;
+            }
+          }
+
+          @keyframes menu-shift-right {
+            from {
+              right: 0;
+            }
+
+            to {
+              right: 250px;
+            }
+          }
+
+          @keyframes menu-unshift-right {
+            from {
+              right: 250px;
+            }
+
+            to {
+              right: 0;
+            }
+          }
+        </style>
+      </nav>
+    </div>
+  </header>
+
+  <h1 style="margin-top: 175px;">PLAYBOY.CC</h1>
+  <a href="https://discord.gg/UH4Uhfp5nJ" class="u-border-2 u-border-white u-btn u-button-style u-btn-1" style="margin: auto;">Discord</a>
+
+    
+
+</body>
